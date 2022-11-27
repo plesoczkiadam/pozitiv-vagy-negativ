b57a526ea52652edc23ed4190d9acf4d0eb9a06a
Ádám Plesoczki <43034243+plesoczkiadam@users.noreply.github.com>
2022-11-27 14:12:36 +0100

    Add files via upload

---
+++ Pozitiv_negativ/Pozitiv_negativ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pozitiv_negativ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double szam;
+            Console.WriteLine("Kérem a számot");
+            Console.Write("szam = ");
+            szam = Convert.ToDouble(Console.ReadLine());
+
+            if (szam > 0) 
+            {
+                Console.WriteLine("{0} szám pózitív", szam);
+            }
+            else
+            {
+                if (szam == 0)
+                {
+                    Console.WriteLine("{0} szám 0", szam);
+                }
+                else
+                {
+                    Console.WriteLine("{0} szám negatív", szam);
+                }
+               
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
